098a0a8f90e99f3b27fcdf6493ad8b9bef402cb5
Erik Tate <hello@eriktate.me>
2021-09-16 17:49:46 -0400

    basic windowing

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,14 @@
 const std = @import("std");
+const window = @import("window.zig");
+const c = @import("c.zig");
 
 pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
+    const win = try window.Window.init(640, 480, "harvest - float");
+    defer win.close();
+
+    // loop forever
+    while (!win.shouldClose()) {
+        c.glClear(c.GL_COLOR_BUFFER_BIT);
+        win.tick();
+    }
 }
