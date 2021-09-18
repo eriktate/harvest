3767b451aec5d470d51b62a7a75746720bf1fff9
Erik Tate <hello@eriktate.me>
2021-09-17 23:06:58 -0400

    adding a triangle to screen to prove everything is actually working

--- src/gl.zig
+++ src/gl.zig
@@ -3,3 +3,14 @@ const c = @import("c.zig");
 pub fn clear() void {
     c.glClear(c.GL_COLOR_BUFFER_BIT);
 }
+
+pub fn genVBO() u32 {
+    var id = 0;
+    c.glGenBuffers(1, &id);
+
+    return id;
+}
+
+pub fn bindArrayBuffer(id: u32) void {
+    c.glBindBuffer(c.GL_ARRAY_BUFFER, id);
+}
--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const window = @import("window.zig");
 const shader = @import("shader.zig");
 const gl = @import("gl.zig");
+const c = @import("c.zig");
 
 pub fn main() anyerror!void {
     const win = try window.Window.init(640, 480, "harvest - float");
@@ -9,10 +10,31 @@ pub fn main() anyerror!void {
 
     const vert_src = @embedFile("../shaders/vertex.glsl");
     const frag_src = @embedFile("../shaders/frag.glsl");
-    _ = try shader.Shader.init(vert_src, frag_src);
+    const sh = try shader.Shader.init(vert_src, frag_src);
+    sh.use();
+
+    const vertices = [_]f32{
+        -0.5, -0.5, 0.0,
+        0.5,  -0.5, 0.0,
+        0.0,  0.5,  0.0,
+    };
+
+    var vao: u32 = 0;
+    c.glGenVertexArrays(1, &vao);
+    c.glBindVertexArray(vao);
+
+    var vbo: u32 = 0;
+    c.glGenBuffers(1, &vbo);
+    c.glBindBuffer(c.GL_ARRAY_BUFFER, vbo);
+    c.glBufferData(c.GL_ARRAY_BUFFER, @sizeOf(f32) * vertices.len, &vertices, c.GL_STATIC_DRAW);
+    c.glVertexAttribPointer(0, 3, c.GL_FLOAT, c.GL_FALSE, 3 * @sizeOf(f32), null);
+    c.glEnableVertexAttribArray(0);
 
     while (!win.shouldClose()) {
         gl.clear();
+        c.glBindVertexArray(vao);
+        c.glDrawArrays(c.GL_TRIANGLES, 0, 3);
+        c.glBindVertexArray(0);
         win.tick();
     }
 }
