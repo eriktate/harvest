116d62aa14f9b2e56ba4b43304a35c30ebd26271
Erik Tate <hello@eriktate.me>
2022-10-29 08:39:28 -0400

    nuking old project because I've rebuilt everything from scratch

--- shaders/frag.glsl
+++
@@ -1,8 +0,0 @@
-#version 330 core
-
-out vec4 frag_color;
-in vec3 out_color;
-
-void main() {
-	frag_color = vec4(out_color, 1.0);
-}
--- src/c.zig
+++
@@ -1,4 +0,0 @@
-pub usingnamespace @cImport({
-    @cInclude("epoxy/gl.h");
-    @cInclude("GLFW/glfw3.h");
-});
